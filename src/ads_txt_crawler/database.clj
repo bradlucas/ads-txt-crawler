d991e6f6ffbb2b50f1113207c9b54b5140a09ad7
Brad Lucas <brad@beaconhill.com>
2017-10-05 12:14:03 -0400

    try catch for SQLiteException
    Inserts will cause exception
    Can log to STDERR
    
    Decided to ignore. In practice a number of Ads.txt files have duplicate rows

--- src/ads_txt_crawler/database.clj
+++ src/ads_txt_crawler/database.clj
@@ -15,12 +15,19 @@
   (let [db-spec (db-spec dbname)]
     (doseq [m data]
       (let [{:keys [exchange-domain account-id account-type tag-id comment]} m]
-        (jdbc/insert! db-spec :adstxt {:site_domain domain
+        ;; some ads.txt files have duplicates
+        ;; for these we can ignore the exception causes by trying to insert a duplicate
+        (try
+          (jdbc/insert! db-spec :adstxt {:site_domain domain
                                        :exchange_domain exchange-domain
                                        :seller_account_id account-id
                                        :account_type account-type
                                        :tag_id tag-id
-                                       :entry_comment comment})))))
+                                         :entry_comment comment})
+          (catch org.sqlite.SQLiteException e
+            ;; Uncomment if you want to see the duplicates
+            ;; (.println *err* (format "Duplicate: %s,%s,%s,%s,%s" domain exchange-domain account-id account-type tag-id))
+            ))))))
 
 
 
