b97e2e8bc10ce3bba13e1c421515545336a9fe0e
Brad Lucas <brad@beaconhill.com>
2017-11-05 16:47:18 -0500

    remove old code

--- src/ads_txt_crawler/crawl.clj
+++ src/ads_txt_crawler/crawl.clj
@@ -7,42 +7,18 @@
   (let [{:keys [exchange-domain account-id account-type tag-id]} record]
     (println (format "%s,%s,%s,%s,%s" domain exchange-domain account-id account-type tag-id))))
 
-;; (defn print-data [domain data]
-;;   (doseq [m data]
-;;     (print-url domain m)))
-
-;; (defn crawl-domain [domain dbname]
-;;   (let [data (p/process domain)]
-;;     (if dbname 
-;;       (data/save domain data dbname)
-;;       (print-data domain data))))
-
-;; (defn crawl-file-opt-database
-;;   "Read the fname for domains and read each for it's ads.txt file and load the contents into dbname"
-;;   ([fname] (crawl-file-opt-database fname nil))
-;;   ([fname dbname]
-;;    (let [domains (d/domains fname)]
-;;      (doseq [domain domains]
-;;        (crawl-domain domain dbname)))))
-
-;; (defn crawl-single-domain
-;;   [domain]
-;;   (print-data domain (p/process (d/clean-domain-name domain))))
-
-
 (defn crawl
   "Crawl a list of domains and process them with the output-fn.
   The output-fn should accept a single parameter which is the return map from process/process-to-map
 
   The domains list can be generated from a file from the domains/domains function.
-  Domains from other sources should be pre-processed with domains/clean-domain-name.
-  "
-  [domains output-fn]
+  Domains from other sources should be pre-processed with domains/clean-domain-name."
+  [domains output-fnc]
   (doseq [d domains]
     (let [data (p/process-to-map d)]
       (if-let [err (:error data)]
-        (println "ERROR: " (:error-message data) (:domain data))
-        (output-fn data)))))
+        (.println *err* (:message data))
+        (output-fnc data)))))
 
 (defn print-results
   "Print the results to stdout"
