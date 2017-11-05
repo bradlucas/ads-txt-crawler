389c645ea3f6ad2a8e63c156ad5d82d826cded7b
Brad Lucas <brad@beaconhill.com>
2017-11-05 16:22:49 -0500

    using new format for calling crawl with input list and output through a function

--- src/ads_txt_crawler/core.clj
+++ src/ads_txt_crawler/core.clj
@@ -1,25 +1,21 @@
 (ns ads-txt-crawler.core
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [ads-txt-crawler.crawl :as c])
+            [ads-txt-crawler.crawl :as c]
+            [ads-txt-crawler.domains :as domains]
+            [ads-txt-crawler.database :as d]
+            )
   (:gen-class))
 
 
-;; default 
-(def targets-file "./doc/target-domains.txt")
-
-
 (def cli-options
-  ;; domain list file
   [["-t" "--targets FILE" "List of domains to crawl ads.txt files from"]
    ["-d" "--database FILE" "Database to dump crawled data into"]])
 
-
 (defn -main [& args]
   (let [opts (parse-opts args cli-options)]
-    (let [fname (or (:targets (:options opts)))
-          database (:database (:options opts))]
-      (if (or fname database)
-        (c/crawl fname database)
-        (doseq [domain args]
-          (c/crawl-single-domain domain))))))
+    (let [targets-file (or (:targets (:options opts)))
+          database-file (:database (:options opts))
+          domains (if targets-file (domains/read-file targets-file) (domains/clean-list args))
+          output-fnc (if database-file (c/save-results database-file) c/print-results)]
+      (c/crawl domains output-fnc))))
 
