a342d6311629da5c95b94951a59d7d739aeea8ec
Brad Lucas <brad@beaconhill.com>
2017-11-05 16:21:06 -0500

    save-record
    New function for saving each record

--- src/ads_txt_crawler/database.clj
+++ src/ads_txt_crawler/database.clj
@@ -7,9 +7,9 @@
    :subname     dbname
    })
 
-(defn save-url [domain url dbname]
-  (let [{:keys [exchange-domain account-id account-type tag-id]} url]
-    (println (format "%s,%s,%s,%s,%s" domain exchange-domain account-id account-type tag-id))))
+;; (defn save-url [domain url dbname]
+;;   (let [{:keys [exchange-domain account-id account-type tag-id]} url]
+;;     (println (format "%s,%s,%s,%s,%s" domain exchange-domain account-id account-type tag-id))))
 
 (defn save [domain data dbname]
   (let [db-spec (db-spec dbname)]
@@ -29,6 +29,23 @@
             ;; (.println *err* (format "Duplicate: %s,%s,%s,%s,%s" domain exchange-domain account-id account-type tag-id))
             ))))))
 
+
+(defn save-record [domain record dbname]
+  (let [db-spec (db-spec dbname)]
+    (let [{:keys [exchange-domain account-id account-type tag-id comment]} record]
+      ;; some ads.txt files have duplicates
+      ;; for these we can ignore the exception causes by trying to insert a duplicate
+      (try
+        (jdbc/insert! db-spec :adstxt {:site_domain domain
+                                       :exchange_domain exchange-domain
+                                       :seller_account_id account-id
+                                       :account_type account-type
+                                       :tag_id tag-id
+                                       :entry_comment comment})
+        (catch org.sqlite.SQLiteException e
+          ;; Uncomment if you want to see the duplicates
+          ;; (.println *err* (format "Duplicate: %s,%s,%s,%s,%s" domain exchange-domain account-id account-type tag-id))
+          )))))
 
 
 
