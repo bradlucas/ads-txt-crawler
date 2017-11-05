77393947a33abe0ab8e892bdac363b478e688299
Brad Lucas <brad@beaconhill.com>
2017-11-05 17:15:35 -0500

    Remove process.clj after merging routines into crawl.clj Refactor process-to-map to get-data

--- src/ads_txt_crawler/crawl.clj
+++ src/ads_txt_crawler/crawl.clj
@@ -1,8 +1,78 @@
 (ns ads-txt-crawler.crawl
-  (:require [ads-txt-crawler.domains :as d]
-            [ads-txt-crawler.database :as data]
-            [ads-txt-crawler.process :as p]))
+  (:require [ads-txt-crawler.database :as d]
+            [ads-txt-crawler.httpkit :as h]))
 
+(defn clean
+  "Clean values by trimming strings and return a blank string to prevent nils from printing"
+  [s]
+  (if s
+    (clojure.string/trim s)
+    ""))
+
+(defn parse-line [line]
+  ;; Examples
+  ;; google.com, pub-1037373295371110, DIRECT #video, banner, native, app
+  ;; pubmatic.com, 120658, Direct, 5d62403b186f2ace
+  (let [[data comment] (clojure.string/split line #"#")]
+    (let [[exchange-domain account-id account-type tag-id] (clojure.string/split (clean data) #",")]
+      {:exchange-domain (clean exchange-domain)
+       :account-id (clean account-id)
+       :account-type (clean account-type)
+       :tag-id (clean tag-id)
+       :comment (clean comment)
+       :data data
+       })))
+
+(defn comment-line [line]
+  (or
+   (clojure.string/starts-with? line "#")
+   (clojure.string/blank? line)))
+
+(defn process-line [line]
+  ;; ignore if commented out line
+  (if (not (comment-line line))
+    (parse-line line)))
+
+(defn is-text [url headers]
+  (try
+    (clojure.string/starts-with? (:content-type headers) "text/plain")
+    (catch java.lang.NullPointerException e
+      (.println *err* (format "Error: content-type is not text/plain for %s" url)))))
+
+(defn is-error [status]
+  (>= status 400))
+  
+(defn build-url [domain]
+  (format "http://%s/ads.txt" domain))
+
+(defn get-data
+  "Read the Ads.txt file for a given domain and return it's data"
+  [domain]
+  (let [url (build-url domain)]
+    ;; read the contents of url
+    ;; - ignore non-text returns
+    ;; - ignore commented lines
+    ;; - parse lines into map of values
+    ;; - ignore
+    (let [rtn {:domain domain}
+          {:keys [status headers body error] :as resp} (h/get-url url)]
+      (if error
+        (assoc rtn :error true :message (format "Error: %s for %s" (.toString error) url))
+        (if status
+          ;; Error returned from server
+          (if (is-error status)
+            (assoc rtn :error true :status status :message (format "Error: 400/500 level error for %s" url))
+            ;; Issue without having headers
+            (if (not headers)
+              (assoc rtn :error true :status status (format "Error: headers are blank for %s" url))
+              ;; Non-text returned
+              (if (not (is-text url headers))
+                (assoc rtn :error true :status status :message (format "Error: non-text result for %s" url))
+                ;; Valid data. Process and return as data
+                (assoc rtn :error false :status status :records (remove nil? (map process-line (clojure.string/split-lines body))))
+                )))
+          ;; No status, No error
+          (assoc rtn :error true :message (format "Error: Unknown issue calling %s" url)))))))
 
 (defn print-results
   "Print the results to stdout"
@@ -21,7 +91,7 @@
     (let [domain (:domain data)
           records (:records data)]
       (doseq [record records]
-        (data/save-record domain record dbname)))))
+        (d/save-record domain record dbname)))))
 
 (defn crawl
   "Crawl a list of domains to crawl and to then out the results with the output-fnc.
@@ -29,8 +99,8 @@
   The domains list can be generated from a file from the domains/domains function.
   Domains from other sources should be pre-processed with domains/clean-domain-name."
   [domains output-fnc]
-  (doseq [d domains]
-    (let [data (p/process-to-map d)]
+  (doseq [ddomain domains]
+    (let [data (get-data domain)]
       (if-let [err (:error data)]
         (.println *err* (:message data))
         (output-fnc data)))))
